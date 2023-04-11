430c3eccdbe8d04b6ddd98d22ff1f8804f7f9e07
Agniya <61684568+agness-anshukova@users.noreply.github.com>
2023-04-11 11:27:18 +0300

    Change selects

--- analitic_select.sql
+++ analitic_select.sql
@@ -7,14 +7,13 @@ group by g.genre_id;
 -- Количество треков, вошедших в альбомы 2019–2020 годов
 select count(at2.track_id)
   from album a 
-  join album_track at2 on a.album_id = at2.album_id 
+  join track at2 on a.album_id = at2.album_id 
   where a."year" between 2019 and 2020;
   
  -- Средняя продолжительность треков по каждому альбому
  select a."name", avg(t.duration)
    from album a 
-   join album_track at2 on a.album_id = at2.album_id 
-   join track t on at2.track_id = t.track_id 
+   join track t on a.album_id = t.album_id  
  group by a."name";
  
 -- Все исполнители, которые не выпустили альбомы в 2020 году
@@ -28,25 +27,40 @@ select p."name"
  						     );
  						    
  -- Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами)
- select c."name" 
+ select c."name"
    from collection c 
-   join collection_track ct on c.collection_id = ct.collection_id 
-   join album_track at2 on ct.track_id = at2.track_id 
-   join album_performer ap on ap.album_id = at2.album_id 
-   join performer p on p.performer_id = ap.performer_id 
-  where p."name" = 'абвгдеёжзийклмнопр';
+   join collection_track ct using(collection_id)
+   join track t using(track_id) 
+   join album a using(album_id)
+  where album_id in (
+  						 select ap.album_id 
+  						   from performer p 
+  						   join album_performer ap on p.performer_id = ap.performer_id 
+  						  where p."name" = 'абвгдеёжз абвг абвгдеёжзийкл' 						    
+  					  );
   
 -- Названия альбомов, в которых присутствуют исполнители более чем одного жанра
-select a."name"
-  from album a,
-       album_performer ap 
- where a.album_id = ap.album_id 
-   and ap.performer_id in  ( select gp.performer_id as performer_id 
+-- count 40.05
+explain analyse  					 
+with cte as  ( select gp.performer_id as performer_id 
           					   from genre_performer gp 
         					   group by gp.performer_id
-        					   having count(gp.genre_id) > 1 ) ;
+        					   having count(gp.genre_id) > 1 ) 
+select (select a."name" from album a where a.album_id = ap.album_id )
+  from album_performer ap 
+  join cte on cte.performer_id = ap.performer_id 
+group by ap.album_id;
 
-        					  
+-- count 63.80
+explain analyse  
+select distinct a."name" 
+  from album a 
+  join album_performer ap on ap.album_id = a.album_id             -- уникальные значения a.album_id (album) связываем с неуникальными значениями ap.album_id (таблица album_performer). Получаем "временную таблицу" с неуникальными  ap.album_id и неуникальными ap.performer_id   
+  join genre_performer gp on gp.performer_id = ap.performer_id    -- неуникальные значения gp.performer_id (genre_performer) связываем с неуникальными значениями ap.performer_id из album_performer ("временная таблица", полученная в результате первого join)
+  join performer p on gp.performer_id = p.performer_id 			  -- неуникальные значения gp.performer_id ( "временная таблица", полученная в результате второго join ) связываем с уникальными значениями p.performer_id 	(performer)	
+group by a."name", p.performer_id 
+having count(gp.genre_id) > 1;
+			  
 -- Наименования треков, которые не входят в сборники
 select t."name" 
   from track t 
@@ -54,31 +68,38 @@ select t."name"
   where ct.collection_id is null;
  
  -- Исполнитель или исполнители, написавшие самый короткий по продолжительности трек
- select p."name"  
-   from album_performer ap,
-        album_track at2,
-        performer p 
-  where ap.album_id = at2.album_id 
-    and ap.performer_id = p.performer_id 
-    and at2.track_id in (
-    						 select t.track_id 
-  							  from track t 
-  							  where t.duration <= (
-  						 						     select min(t.duration) 
-  						 							 from track t
-  					  							  )
-    					);
-    
--- Названия альбомов, содержащих наименьшее количество треков
-select ( select a."name" from album a where a.album_id = at3.album_id )
-  from album_track at3 
-  group by at3.album_id 
-  having count(at3.track_id) = (
-  									select  min(t.cnt)
-   									  from (
-		 									 select count(at2.track_id) as cnt
-   		   									  from album_track at2  
-   		   									  group by at2.album_id 
-										  ) t
-								);
+ -- Вариант 1 
+ explain analyse
+ select p."name" 
+   from performer p 
+   join album_performer ap on p.performer_id = ap.performer_id   -- уникальные значения (p.performer_id) с неуникальными (ap.performer_id). "Временная таблица" с неуникальными (ap.album_id)
+   join album a on a.album_id = ap.album_id 					 -- уникальные значения (a.album_id) с неуникальными (ap.album_id). "Временная таблица" с неуникальными (ap.album_id)
+   join track t on t.album_id = ap.album_id 					 -- неуникальные значения (t.album_id) с неуникальными (ap.album_id )
+   where t.duration = (select min(t2.duration) from track t2 );
+ 
+ -- Вариант 2
+ explain analyse 	
+ select p."name" 
+   from album_performer ap     									 
+   join performer p on p.performer_id = ap.performer_id 		 -- уникальные значения performer_id из performer с неуникальными значениям ap.performer_id из album_performer
+where ap.album_id in (											 -- уникальные значения t.album_id 	
+						select t.album_id                     
+  						  from track t 			 
+ 						 where t.duration = (select min(t2.duration) from track t2) 
+ 						 group by t.album_id 
+					 );
+  
 
+  
+-- Названия альбомов, содержащих наименьшее количество треков
+select a2."name" 
+  from album a2 
+  join track t on t.album_id = a2.album_id  
+  group by a2.album_id
+  having count(t.track_id) = (
+  								select count(t2.track_id)
+  								  from track t2 
+  								 group by t2.album_id 
+  								 order by 1
+  								 limit 1
+ 							 );
